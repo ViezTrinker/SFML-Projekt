86133e2b89740c2ac60edb2571e09d5351635afb
ViezTrinker <muham@DESKTOP-PKQMJ32>
2023-04-16 00:33:53 +0200

    Bouncing mushroom

--- SFML-Projekt/main.cpp
+++ SFML-Projekt/main.cpp
@@ -2,14 +2,14 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(640, 480), "Rendering the rectangle");
+	sf::RenderWindow window(sf::VideoMode(640, 480), "Bouncing Mushroom");
 
-	// Creating our shape
-	sf::RectangleShape rectangle(sf::Vector2f(128.0f, 128.0f));
-	rectangle.setFillColor(sf::Color::Red);
-	rectangle.setPosition(320, 240);
-	rectangle.setOrigin(rectangle.getSize().x / 2, rectangle.getSize().y / 2);
-
+	sf::Texture mushroomTexture;
+	mushroomTexture.loadFromFile("mushroom.png");
+	sf::Sprite mushroom(mushroomTexture);
+	sf::Vector2u size = mushroomTexture.getSize();
+	mushroom.setOrigin(size.x / 2, size.y / 2);
+	sf::Vector2f increment(0.4f, 0.4f);
 
 	while (window.isOpen())
 	{
@@ -18,14 +18,29 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 			{
-				// Close window button clicked
 				window.close();
 			}
 		}
 
-		window.clear(sf::Color::Black);
-		window.draw(rectangle);
-		window.display();
+		if (mushroom.getPosition().x + (size.x / 2) > window.getSize().x && increment.x > 0 ||
+			(mushroom.getPosition().x - (size.x / 2) < 0 && increment.x < 0))
+		{
+			// Reverse the direction on x-Axis
+			increment.x = -increment.x;
+		}
 
+		if (mushroom.getPosition().y + (size.y / 2) > window.getSize().y && increment.y > 0 ||
+			(mushroom.getPosition().y - (size.y / 2) < 0 && increment.y < 0))
+		{
+			// Reverse the direction on y-Axis
+			increment.y = -increment.y;
+		}
+
+		mushroom.setPosition(mushroom.getPosition() + increment);
+
+		window.clear(sf::Color(16, 16, 16, 255)); // Dark gray
+		window.draw(mushroom); // Drawing our Sprite
+		window.display();
 	}
+	return 0;
 }
