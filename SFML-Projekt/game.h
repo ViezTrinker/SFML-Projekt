64a3f53d48f9c7bf774eed3887875dddeef52e67
ViezTrinker <muham@DESKTOP-PKQMJ32>
2023-04-16 21:47:19 +0200

    Implemented Game Class

---
+++ SFML-Projekt/game.h
@@ -0,0 +1,19 @@
+#include <window.h>
+
+class Game {
+public:
+	Game();
+	~Game();
+
+	//void HandleInput();
+	void Update();
+	void Render();
+	Window* GetWindow();
+
+private:
+	void MoveMushroom();
+	Window m_window;
+	sf::Texture m_mushroomTexture;
+	sf::Sprite m_mushroom;
+	sf::Vector2i m_increment;
+};
--- SFML-Projekt/main.cpp
+++ SFML-Projekt/main.cpp
@@ -1,47 +1,16 @@
-#include <SFML/Graphics.hpp>
-#include <window.h>
+#include <game.h>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(640, 480), "Bouncing Mushroom");
-
-	sf::Texture mushroomTexture;
-	mushroomTexture.loadFromFile("mushroom.png");
-	sf::Sprite mushroom(mushroomTexture);
-	sf::Vector2u size = mushroomTexture.getSize();
-	mushroom.setOrigin(size.x / 2, size.y / 2);
-	sf::Vector2f increment(0.4f, 0.4f);
-
-	while (window.isOpen())
+	// Program entry point
+	Game game; // Creating our game object
+	while (!game.GetWindow()->IsDone())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-			{
-				window.close();
-			}
-		}
-
-		if (mushroom.getPosition().x + (size.x / 2) > window.getSize().x && increment.x > 0 ||
-			(mushroom.getPosition().x - (size.x / 2) < 0 && increment.x < 0))
-		{
-			// Reverse the direction on x-Axis
-			increment.x = -increment.x;
-		}
-
-		if (mushroom.getPosition().y + (size.y / 2) > window.getSize().y && increment.y > 0 ||
-			(mushroom.getPosition().y - (size.y / 2) < 0 && increment.y < 0))
-		{
-			// Reverse the direction on y-Axis
-			increment.y = -increment.y;
-		}
-
-		mushroom.setPosition(mushroom.getPosition() + increment);
-
-		window.clear(sf::Color(16, 16, 16, 255)); // Dark gray
-		window.draw(mushroom); // Drawing our Sprite
-		window.display();
+		// Game Loop
+		//game.HandleInput();
+		game.Update();
+		game.Render();
 	}
+
 	return 0;
 }
